7286905b2e060de335d5204f559e089bc8009000
Ciclica <ciclica@Ciclicas-Mac.local>
2016-01-04 20:56:45 -0300

    Initial Commit

---
+++ SecuenciaFibonacci/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  SecuenciaFibonacci
+//
+//  Created by Ciclica on 1/4/16.
+//  Copyright (c) 2016 Ciclica. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
